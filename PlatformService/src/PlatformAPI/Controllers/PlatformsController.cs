9418780b1c2a9e4908652b875f0a7b41b39fb3d8
None <None@noreply>
2022-02-14 16:57:25 -0300

    Add controllers

---
+++ PlatformService/src/PlatformAPI/Controllers/PlatformsController.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformApi.Data;
+using PlatformApi.Dtos;
+using PlatformApi.Models;
+
+namespace PlatformApi.Controllers
+{
+    [ApiController]
+    [Route("api/platforms")]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet] // GET /platformts
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            var p = _repository.GetAllPlatforms();
+
+            if (!p.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(p));
+        }
+
+        [HttpGet("{id}")] // GET /platformts/{id}
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var p = _repository.GetPlatformById(id);
+
+            if (p is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<PlatformReadDto>(p));
+        }
+
+        [HttpPost] // POST /items
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto request)
+        {
+            var p = _mapper.Map<Platform>(request);
+
+            _repository.CreatePlatform(p);
+            _repository.SaveChanges();
+
+            return CreatedAtAction(nameof(GetPlatformById), new{id = p.Id}, _mapper.Map<PlatformReadDto>(p));
+        }
+    }
+}
