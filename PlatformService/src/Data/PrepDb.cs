9acece1138a34432e5691b39b63d7631fd338605
None <None@noreply>
2022-03-05 15:52:25 -0300

    Add db migrations

--- PlatformService/src/Data/PrepDb.cs
+++ PlatformService/src/Data/PrepDb.cs
@@ -1,19 +1,33 @@
+using Microsoft.EntityFrameworkCore;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool IsProduction)
         {
             using ( var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());    
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), IsProduction);    
             }
         }
 
-        public static void SeedData(AppDbContext context)
+        public static void SeedData(AppDbContext context, bool IsProduction)
         {
+            if(IsProduction)
+            {
+                Console.WriteLine("--> Trying to apply migrations");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not migrate: {ex.Message}");
+                }
+            }
+
             if(!context.Platforms.Any())
             {
                 Console.WriteLine("--> Seeding data");
