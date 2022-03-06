5ab95b64fea886379d06bce003ca597b8c4e9335
None <None@noreply>
2022-03-05 21:21:27 -0300

    Update variables names

--- PlatformService/src/Settings/EnvironmentVariables.cs
+++ PlatformService/src/Settings/EnvironmentVariables.cs
@@ -4,11 +4,11 @@ namespace PlatformService.Settings
     {
         public string GetConnectionString()
         {
-            var server = Environment.GetEnvironmentVariable("SQL_SERVER_URL");
-            var port = Environment.GetEnvironmentVariable("SQL_SERVER_PORT");
-            var user = Environment.GetEnvironmentVariable("SQL_SERVER_USER");
-            var password = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD");
-            var database = Environment.GetEnvironmentVariable("SQL_SERVER_DB_NAME");
+            var server = Environment.GetEnvironmentVariable("PLATFORM_SQL_SERVER_URL");
+            var port = Environment.GetEnvironmentVariable("PLATFORM_SQL_SERVER_PORT");
+            var user = Environment.GetEnvironmentVariable("PLATFORM_SQL_SERVER_USER");
+            var password = Environment.GetEnvironmentVariable("PLATFORM_SQL_SERVER_PASSWORD");
+            var database = Environment.GetEnvironmentVariable("PLATFORM_SQL_SERVER_DB_NAME");
             return $"Server={server},{port}; Initial Catalog={database}; User ID={user}; Password={password}";
         }
 
