4576c65de494e214e362a3add5a6952ace2a23b4
Wuerike <wuerike.henri@gmail.com>
2022-04-17 17:36:12 -0300

    ps: Fix controllers unit tests

--- PlatformService/tests/unit/Controllers/PlatformsControllerTests.cs
+++ PlatformService/tests/unit/Controllers/PlatformsControllerTests.cs
@@ -1,21 +1,26 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PlatformService.Controllers;
 using PlatformService.Data;
+using PlatformService.DataServices.Async;
+using PlatformService.DataServices.Sync.Http;
 using PlatformService.Dtos;
 using PlatformService.Models;
 using PlatformService.Profiles;
 using Xunit;
 
-namespace PlatformServiceUnitTests;
+namespace PlatformServiceUnitTests.Controllers;
 
 public class PlatformsControllerTests
 {
     private readonly Mock<IPlatformRepo> repoMock = new();
+    private readonly Mock<ICommandDataClient> commandDataClientMock = new();
+    private readonly Mock<IMessageBusClient> messageBusClient = new();
     private readonly Random Rand = new();
     private readonly IMapper _mapper;
 
@@ -55,7 +60,7 @@ public class PlatformsControllerTests
         var expectedPlatform = new[]{CreateRandomPlatform(), CreateRandomPlatform(), CreateRandomPlatform()};
         
         repoMock.Setup(repo => repo.GetAllPlatforms()).Returns(expectedPlatform);
-        var controller = new PlatformsController(repoMock.Object, _mapper);
+        var controller = new PlatformsController(repoMock.Object, _mapper, commandDataClientMock.Object, messageBusClient.Object);
 
         var result = controller.GetPlatforms();
         result.Value.Should().BeEquivalentTo(expectedPlatform);
@@ -65,7 +70,7 @@ public class PlatformsControllerTests
     public void GetPlatforms_WithNullPlatform_ReturnsNotFound()
     {       
         repoMock.Setup(repo => repo.GetAllPlatforms()).Returns(Enumerable.Empty<Platform>());
-        var controller = new PlatformsController(repoMock.Object, _mapper);
+        var controller = new PlatformsController(repoMock.Object, _mapper, commandDataClientMock.Object, messageBusClient.Object);
 
         var result = controller.GetPlatforms();
         result.Result.Should().BeOfType<NotFoundResult>();
@@ -77,7 +82,7 @@ public class PlatformsControllerTests
         var expectedPlatform = CreateRandomPlatform();
 
         repoMock.Setup(repo => repo.GetPlatformById(It.IsAny<int>())).Returns(expectedPlatform);
-        var controller = new PlatformsController(repoMock.Object, _mapper);
+        var controller = new PlatformsController(repoMock.Object, _mapper, commandDataClientMock.Object, messageBusClient.Object);
 
         var result = controller.GetPlatformById(Rand.Next(100));
         result.Value.Should().BeEquivalentTo(expectedPlatform);
@@ -87,21 +92,41 @@ public class PlatformsControllerTests
     public void GetPlatformById_WithNullPlatform_ReturnsNotFound()
     {
         repoMock.Setup(repo => repo.GetPlatformById(It.IsAny<int>())).Returns((Platform)null);
-        var controller = new PlatformsController(repoMock.Object, _mapper);
+        var controller = new PlatformsController(repoMock.Object, _mapper, commandDataClientMock.Object, messageBusClient.Object);
 
         var result = controller.GetPlatformById(Rand.Next(100));
         result.Result.Should().BeOfType<NotFoundResult>();
     }
 
     [Fact]
-    public void CreatePlatform_ReceivingPlatform_ReturnsCreatedPlatform()
+    public async Task CreatePlatform_ReceivingPlatform_ReturnsCreatedPlatform()
     {
         var platformToCreate = CreateRandomPlatformCreateDto();
 
-        var controller = new PlatformsController(repoMock.Object, _mapper);
+        repoMock.Setup(repo => repo.CreatePlatform(It.IsAny<Platform>()));
+        commandDataClientMock.Setup(repo => repo.SendPlatformToCommand(It.IsAny<PlatformReadDto>()));
+        messageBusClient.Setup(repo => repo.PublishNewPlatform(It.IsAny<PlatformPublishDto>()));
+        var controller = new PlatformsController(repoMock.Object, _mapper, commandDataClientMock.Object, messageBusClient.Object);
 
-        var result = controller.CreatePlatform(platformToCreate);
-        var createdPlatform= (result.Result as CreatedAtActionResult).Value as PlatformReadDto;
+        var result = await controller.CreatePlatform(platformToCreate);
+        var createdPlatform = (result.Result as CreatedAtActionResult).Value as PlatformReadDto;
+
+        createdPlatform.Should().BeEquivalentTo(platformToCreate);
+        createdPlatform.Id.Should().BeOfType(typeof(int));
+    }
+
+    [Fact]
+    public async Task CreatePlatform_ReceivingPlatform_ExternalClientsWithError_ReturnsCreatedPlatform()
+    {
+        var platformToCreate = CreateRandomPlatformCreateDto();
+
+        repoMock.Setup(repo => repo.CreatePlatform(It.IsAny<Platform>()));
+        commandDataClientMock.Setup(repo => repo.SendPlatformToCommand(It.IsAny<PlatformReadDto>())).Throws(new Exception());
+        messageBusClient.Setup(repo => repo.PublishNewPlatform(It.IsAny<PlatformPublishDto>())).Throws(new Exception());
+        var controller = new PlatformsController(repoMock.Object, _mapper, commandDataClientMock.Object, messageBusClient.Object);
+
+        var result = await controller.CreatePlatform(platformToCreate);
+        var createdPlatform = (result.Result as CreatedAtActionResult).Value as PlatformReadDto;
 
         createdPlatform.Should().BeEquivalentTo(platformToCreate);
         createdPlatform.Id.Should().BeOfType(typeof(int));
